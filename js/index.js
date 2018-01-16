4114ab5035df88a0aa2fbd9a6d6ef98547e73130
tomascayul <cayul.tomas@gmail.com>
2018-01-16 11:40:44 -0300

    formulario funcionando y guardando en un arreglo mediante JSON

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 // esto es para asegurarnos de que se cargue completamente el HTML
 $(document).ready(function () {
 
-	$('#inputForm').on('submit',function(event){
-		event.preventDefault();
-	});
 	
 	// arreglo donde se guardaran los datos del formulario
 	var users = [];
@@ -28,15 +25,32 @@ $(document).ready(function () {
 		user.city = $(this).val();
 	});
 
-    $('#btnEnviar').on('click', function () {
+	$('#btnEnviar').on('click', function () {
 
-	// rellenamos el array users
-    users.push(user.name, user.email, user.city);
+		// rellenamos el array users
+		var us = JSON.parse(JSON.stringify(user));
+		users.push(us);
+	    // users.push(user.name, user.email, user.city);
 
-	console.log(users);
+		console.log(users);
 
 	});  
 
+	
+ /*   $('#btnEnviar').on('click', function () {
+
+	$('#inputForm').on('submit',function(event){
+		event.preventDefault();
+		user.email = $('#email').val();
+		user.name = $('#name').val();
+		user.city = $('#city').val();
+		var us = JSON.parse(JSON.stringify(user));
+		users.push(us);
+		
+		});
+
+	});*/
+	// console.log(users); 
 
 
 
